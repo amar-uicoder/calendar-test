5b2ba1f54b325d9bd9cbfbb223916946f3494090
amar <amar.uicoder@gmail.com>
2024-01-30 00:23:37 +0530

    Static

--- app/[year]/[month]/page.tsx
+++ app/[year]/[month]/page.tsx
@@ -1,4 +1,7 @@
 import Calendar from "../../component/calendar/calendar";
+export function generateStaticParams() {
+    return [{ year: '2024', month: '1' }, { year: '2024', month: '2' }]
+  }
 
 export default function Page({ params }: { params: { year: number, month: number } }) {
     return <Calendar year={params.year} month={params.month} />
